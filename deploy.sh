d78077522954a7c55c9d2d7b64a1ba137dea1c1a
bazoookia <bazoookia@gmail.com>
2024-03-02 00:01:18 +0800

    修复github page中文问题

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,33 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+find notebook -name "*.md" -not -name "README.md" -exec md5sum {} \; > name_mapping.file
+while read file_md5 file_path_name
+do
+    mv ${file_path_name} `dirname ${file_path_name}`/${file_md5}.md
+done < name_mapping.file
+
 # 生成静态文件
 yarn b
-
+ 
 # 进入生成的文件夹
 cd .vuepress/public
-
+ 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
-
+ 
 git init
 git add -A
 git commit -m 'deploy'
-
+ 
 git push -f git@github.com:bazoookia/bazoookia.github.io.git master
-
+ 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
+ 
 cd -
+
+while read file_md5 file_path_name
+do
+    mv `dirname ${file_path_name}`/${file_md5}.md ${file_path_name}
+done < name_mapping.file
